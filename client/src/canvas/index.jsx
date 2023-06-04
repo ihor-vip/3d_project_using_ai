3c46b5c5bfdbc5fea554afeca04d306692bbab18
ihor-vip <vip20140902@gmail.com>
2023-06-04 10:44:57 +0300

    create 3d canvas

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,7 +1,24 @@
-export default function Canvas() {
+import { Canvas } from '@react-three/fiber'
+import { Environment, Center } from '@react-three/drei';
+
+import Shirt from './Shirt';
+import Backdrop from './Backdrop';
+import CameraRig from './CameraRig';
+
+const CanvasModel = () => {
     return (
-        <div>
-            canvas
-        </div>
+        <Canvas>
+            <ambientLight intensity={0.5} />
+            <Environment preset="city" />
+
+            {/*<CameraRig>*/}
+            {/*    <Backdrop />*/}
+                <Center>
+                    <Shirt />
+                </Center>
+            {/*</CameraRig>*/}
+        </Canvas>
     )
 }
+
+export default CanvasModel

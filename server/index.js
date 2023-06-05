5370fbc52409d00113c3cf81c42ac3c227a201e9
ihor-vip <vip20140902@gmail.com>
2023-06-05 22:02:35 +0300

    create backend part ai from openai

--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
-import express from "express";
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
 
-const port = 8080;
+import dalleRoutes from './routes/dalle.routes.js';
+
+dotenv.config();
+
 const app = express();
+app.use(cors());
+app.use(express.json({ limit: "50mb" }))
 
-app.get('/', (req,res) => res.send('Server ready'));
+app.use("/api/v1/dalle", dalleRoutes);
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.get('/', (req, res) => {
+    res.status(200).json({ message: "Hello from DALL.E" })
+})
+
+app.listen(8080, () => console.log('Server has started on port 8080'))
